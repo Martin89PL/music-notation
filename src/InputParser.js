509e382ae9deaded23329e7164c76da34678fa89
Marcin <marcingladkowski@gmail.com>
2019-03-23 14:00:35 +0100

    refactor(parser): using map function

--- src/InputParser.js
+++ src/InputParser.js
@@ -20,31 +20,23 @@ class InputParser {
      * @param {string} line 
      */
     static parseLine(line) {
+        
         line = InputParser.removeStrategyName(line);
-        let parsed = line.split('|').map(note => note.trim());
-        let notesLine = [];
-
-        for(let parse of parsed) {
-            let result = parse.match(/repeat/g);
-            if(result != null && result[0] === 'repeat') {
-                let howMany = InputParser.repeat(parse);
-                for(let i = 0; i < howMany; i++) {
-                    notesLine.push(InputParser.parseNoteNotation(parse));
-                }
-                continue;
-            }
-            notesLine.push(InputParser.parseNoteNotation(parse));
-        }
-
-        return notesLine;
+        return line.split('|')
+                .map(note => note.trim())
+                .map(note => InputParser.parseNoteNotation(note));
     }
+
     /**
      * 
      * @param {string} note 
      */
     static parseNoteNotation(note) {
         let values = note.match(/\((.*?)\)/g)[0].replace('(', '').replace(')', '');
-        return values.split(',').map(value => value.trim());
+        
+        return values.split(',')
+                .map(note => note.trim())
+                .map(note => note.split(';'))
     }
     
     /**
@@ -52,8 +44,7 @@ class InputParser {
      * @param {string} line 
      */
     static repeat(line) {
-        let result =  line.split(' ', 3);
-        return result[1];
+        return line.split(' ', 5);
     }
     /**
      * Return function name
